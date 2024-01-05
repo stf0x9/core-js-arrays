3a9a01c5950ae8a7a5797113af6ae97359cf553c
stf0x9 <stf0x9@gmail.com>
2024-01-05 14:25:55 -0800

    feat: add solution generateOdds

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -368,8 +368,8 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return Array.from({ length: len }, (v, k) => k * 2 + 1);
 }
 
 /**
