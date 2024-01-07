bff788a1142ecb042f91a800af27c9e91cf13abc
stf0x9 <stf0x9@gmail.com>
2024-01-07 10:43:59 -0800

    feat: add solution getIdentityMatrix

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -422,8 +422,14 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  const arr = Array(n).fill(0);
+  return arr.reduce((acc, curr, i) => {
+    const row = Array(n).fill(0);
+    row[i] = 1;
+    acc.push(row);
+    return acc;
+  }, []);
 }
 
 /**
